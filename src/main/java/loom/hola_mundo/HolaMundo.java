fcf5ebcc70c3d56c5ced8571ba59c6d1bd3722a7
OviedoLeandro <lea_oviedo@hotmail.com>
2018-07-13 12:01:36 -0300

    Hola Mundo v1

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -2,7 +2,7 @@ package loom.hola_mundo;
 
 public class HolaMundo {
 
-    public String saludar(final String persona) {
-        throw new RuntimeException("¡Implementar este método!");
-    }
+	public String saludar(final String persona) {
+		return "¡Hola, " + persona + "!";
+	}
 }
